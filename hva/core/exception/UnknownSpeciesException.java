a35b63fc70e2f44cbfb02bd157027c092554cc07
b-Spedras <bernardo.s.pederneira@tecnico.ulisboa.pt>
2024-10-11 20:26:24 +0100

    Added exceptions

---
+++ hva/core/exception/UnknownSpeciesException.java
@@ -0,0 +1,15 @@
+package hva.core.exception;
+
+import java.exception;
+
+import java.io.Serial;
+
+public class UnknownSpeciesException extends Exception {
+  @Serial
+  private static final long serialVersionUID = 202407081733L;
+
+ /*
+   public UnknownSpeciesException(String key) {
+    super(Message.unknownSpeciesKey(key));
+  } */
+}
