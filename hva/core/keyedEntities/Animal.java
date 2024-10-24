56165396ee217481a2b8834bd907b3e1c2cb573e
Tiago Ferreira <tiagofer2004@gmail.com>
2024-10-24 13:00:09 +0100

    Update Animal.java

--- hva/core/keyedEntities/Animal.java
+++ hva/core/keyedEntities/Animal.java
@@ -1,15 +1,17 @@
 package hva.core.keyedEntities;
 import java.util.ArrayList;
 
+import hva.core.enums.HealthState;
+
 public class Animal extends KeyedEntity{
-    private ArrayList<String> _healthState;
+    private ArrayList<HealthState> _healthState;
     private ArrayList<VaccineRegistry> _vaccines;
     private Habitat _habitat;
     private Species _species; //Achas que em vez de uma especie pode levar um speciesID?
 
     public Animal(String key, String name, Species species, Habitat habitat){
         super(key, name);
-        _healthState = new ArrayList<String>();
+        _healthState = new ArrayList<HealthState>();
         _vaccines = new ArrayList<VaccineRegistry>();
         _species = species;
         _habitat = habitat;
@@ -19,13 +21,18 @@ public class Animal extends KeyedEntity{
         return ("ANIMAL|" + super.toString() + "|" + _species.getKey() + printSaude() + "|" + _habitat.getKey());
     }
 
+    public void changeHabitat(Habitat hab){
+        _habitat = hab;
+    }
+
+
 
 
     private String printSaude(){
         if (_healthState.isEmpty()){return ("|VOID");}
         String s = new String();
-        for (String string: _healthState){
-            s = s + "," + string;
+        for (HealthState h: _healthState){
+            s = s + "," + h.toString();
         }
         return ("|" + s.substring(1));
     }
