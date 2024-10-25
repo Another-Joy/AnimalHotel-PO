4ad14659ba1b5f751c9d5f822f8f07403bd97c94
Tiago Ferreira <tiagofer2004@gmail.com>
2024-10-25 17:18:03 +0100

    Update LeafState.java

--- hva/core/enums/LeafState.java
+++ hva/core/enums/LeafState.java
@@ -1,8 +1,20 @@
 package hva.core.enums;
 
 public enum LeafState{
-    NO,
-    GENERATE,
-    WITH,
-    DROP
+    NO("SEMFOLHAS"),
+    GENERATE("GERARFOLHAS"),
+    WITH("COMFOLHAS"),
+    DROP("LARGARFOLHAS");
+
+
+    private String _str;
+
+    private LeafState(String str){
+        _str = str;
+    }
+
+    public String toString(){
+        return _str;
+    }
+
 }
