619749dda86ff22774ef7c91bd8edf29c5571225
b-Spedras <bernardo.s.pederneira@tecnico.ulisboa.pt>
2024-10-25 23:12:55 +0100

    code look slightly pretier (on Show all trees)

--- hva/app/habitat/DoShowAllTreesInHabitat.java
+++ hva/app/habitat/DoShowAllTreesInHabitat.java
@@ -3,11 +3,10 @@ package hva.app.habitat;
 import hva.app.exception.UnknownHabitatKeyException;
 import hva.core.Hotel;
 import hva.core.exception.UnknownHabitatException;
-import pt.tecnico.uilib.forms.Field;
 import pt.tecnico.uilib.menus.Command;
 import pt.tecnico.uilib.menus.CommandException;
 import java.util.stream.Collectors;
-//FIXME add more imports if needed
+
 
 /**
  * Show all trees in a given habitat.
@@ -23,7 +22,6 @@ class DoShowAllTreesInHabitat extends Command<Hotel> {
   @Override
   protected void execute() throws CommandException {
     try {
-      // _receiver.showTrees(stringField("idHabitat"));
       _display.popup(_receiver.showTrees(stringField("idHabitat")).stream().sorted().collect(Collectors.toList()));
     } catch (UnknownHabitatException ex) {
       throw new UnknownHabitatKeyException(ex.getID());
