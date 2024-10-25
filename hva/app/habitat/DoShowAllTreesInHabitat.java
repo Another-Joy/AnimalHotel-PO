ef64f892642a28c2cc5b35f388c6dd2c30ba0b5e
b-Spedras <bernardo.s.pederneira@tecnico.ulisboa.pt>
2024-10-25 22:57:06 +0100

    DoShowAllTrees implement

--- hva/app/habitat/DoShowAllTreesInHabitat.java
+++ hva/app/habitat/DoShowAllTreesInHabitat.java
@@ -3,8 +3,10 @@ package hva.app.habitat;
 import hva.app.exception.UnknownHabitatKeyException;
 import hva.core.Hotel;
 import hva.core.exception.UnknownHabitatException;
+import pt.tecnico.uilib.forms.Field;
 import pt.tecnico.uilib.menus.Command;
 import pt.tecnico.uilib.menus.CommandException;
+import java.util.stream.Collectors;
 //FIXME add more imports if needed
 
 /**
@@ -21,9 +23,10 @@ class DoShowAllTreesInHabitat extends Command<Hotel> {
   @Override
   protected void execute() throws CommandException {
     try {
-      // _receiver.showAllTrees
+      // _receiver.showTrees(stringField("idHabitat"));
+      _display.popup(_receiver.showTrees(stringField("idHabitat")).stream().sorted().collect(Collectors.toList()));
     } catch (UnknownHabitatException ex) {
-      throw new UnknownHabitatKeyException(ex.getID())
+      throw new UnknownHabitatKeyException(ex.getID());
     }
   }
 }
